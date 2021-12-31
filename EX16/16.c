25226cfc40e57ce4882618c42a9fd7958e50742c
ShuMay-Sky <94574508+ShuMay-Sky@users.noreply.github.com>
2021-12-31 10:41:45 +0900

    31th2, dec

--- EX16/16.c
+++ EX16/16.c
@@ -5,6 +5,6 @@ int main(void)
 	int san = 93; /* 算数の得点 */
 	int sya = 68; /* 社会の得点 */
 	printf("国語は%d点、算数は%d点、社会は%d点です。\n", koku, san, sya);
-	printf("平均点は%.1f点です。\n", (double)(koku + san + sya) / 3);
+	printf("平均点は%d点です。\n", (int)((double)(koku + san + sya) / 3 + 0.5));
 	return 0;
 }
