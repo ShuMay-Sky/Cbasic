8ab66d27b78910586736c13370734d28e0ca6433
ShuMay-Sky <94574508+ShuMay-Sky@users.noreply.github.com>
2022-01-02 21:17:26 +0900

    2-1, jan

---
+++ EX19/EX19.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+	int i;
+	printf("³‚Ì®”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢ > ");
+	scanf("%d", &i);
+	if (i % 2)
+		printf("%d‚ÍŠï”‚Å‚·B\n", i);
+	else
+		printf("%d‚Í‹ô”‚Å‚·B\n", i);
+	return 0;
+}
---
+++ EX20/EX20.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	int i;
+	printf("³‚Ì®”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢ > ");
+	scanf("%d", &i);
+	if (i % 2)
+		printf("%d‚ÍŠï”‚Å‚·B\n", i);
+	return 0;
+}
